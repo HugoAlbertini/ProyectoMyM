99cbcc08f34fc5ae89cc52455e45e8798a8de843
volcanicdoggg-max <volcanicdoggg@gmail.com>
2026-05-07 23:40:16 -0300

    fix: Gallery grid on mobile + CaseStudy double scroll issue

--- proyectomym/src/components/sections/CaseStudy.jsx
+++ proyectomym/src/components/sections/CaseStudy.jsx
@@ -26,6 +26,7 @@ const CaseStudy = ({ item, onClose }) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
+      data-lenis-prevent
     >
       <div className="case-study-container">
         
